2eec8ef6a460e090be75d4c723092de78b959b82
James Gliwa <jgliwa12@gmail.com>
2019-10-02 22:48:12 -0700

    more and more

--- Source/content_script.js
+++ Source/content_script.js
@@ -47,14 +47,20 @@ function replaceText(v)
     v = v.replace(/\bgo vegan\b/g, "be a good person");
     v = v.replace(/\bGoing vegan\b/g, "Being a good person");
     v = v.replace(/\bgoing vegan\b/g, "being a good person");
-    v = v.replace(/\bBe (V|v)egan\b/g, "Be a good person");
-    v = v.replace(/\bbe vegan\b/g, "be a good person");
     v = v.replace(/\bEat vegan\b/g, "Eat without causing others harm");
     v = v.replace(/\beat vegan\b/g, "eat without causing others harm");
+    v = v.replace(/\bgo vegan\b/g, "be a good person");
+    v = v.replace(/\b(is|am|be|I'm|im|i'm|Be) (V|v)egan\b/g, "$1 a good person");
+    v = v.replace(/\bI("|')M VEGAN\b/g, "I'M A GOOD PERSON");
+    v = v.replace(/\bturn vegan\b/g, "become enlightened");
 
-    // Definition
+    // Definitions
     v = v.replace(/\bveg·an\b/g, "mor·al·ly su·pe·ri·or per·son");
     v = v.replace(/\bvēɡən\b/g, "môrəlē səˈpirēər pərs(ə)n");
+    v = v.replace(/\bcar·ni·vore\b/g, "blood·mouth");
+    v = v.replace(/\bkärnəˌvôr\b/g, "blədmouTH");
+    v = v.replace(/\bveg·e·tar·i·an\b/g, "cheese·breath·er");
+    v = v.replace(/\bvejəˈterēən\b/g, "CHēzˈbrēT͟Hər");
 
     // Types of Vegoofballs
     v = v.replace(/\bStrict vegan(?:(s)\b|(')s\b)?/g, "Low self-monitor2$1");
@@ -72,16 +78,10 @@ function replaceText(v)
     v = v.replace(/\bCheat day(s)?\b/g, "Time$1 of weakness");
     v = v.replace(/\bcheat day(s)?\b/g, "time$1 of weakness");
 
-    // Vegan
-    v = v.replace(/\bVegan\b/g, "Morally Superior Person");
-    v = v.replace(/\bvegan\b/g, "morally superior person");
-    v = v.replace(/\bVegan(?:(s)\b(')|s\b)/g, "Morally Superior People$2$1");
-    v = v.replace(/\bvegan(?:(s)\b(')|s\b)/g, "morally superior people$2$1");
-
     // meat
     v = v.replace(/\bMeat(s)?\b/g, "Dead Animal Flesh");
     v = v.replace(/\bmeat(s)?\b/g, "dead animal flesh");
-    v = v.replace(/\bBeef\b/g, "Dead Bow");
+    v = v.replace(/\bBeef\b/g, "Dead Cow");
     v = v.replace(/\bbeef\b/g, "dead cow");
     v = v.replace(/\bPork\b/g, "Dead Pig");
     v = v.replace(/\bPork (B|b)elly\b/g, "Dead Pig Stomach");
@@ -89,22 +89,51 @@ function replaceText(v)
     v = v.replace(/\bpork\b/g, "dead pig");
     v = v.replace(/\bBacon\b/g, "Salt-cured Dead Pig");
     v = v.replace(/\bbacon\b/g, "salt-cured dead pig");
+    v = v.replace(/\bHam\b/g, "Dead Pig Leg");
+    v = v.replace(/\bham\b/g, "dead pig leg");
     v = v.replace(/\bSausage(s)?\b/g, "Dead Animal Mélange$1");
-    v = v.replace(/\bsausage(s)?\b/g, "dead animal mélange$1");    
+    v = v.replace(/\bsausage(s)?\b/g, "dead animal mélange$1");
+    v = v.replace(/\bSalami(s)?\b/g, "Cured Dead Animal Mélange$1");
+    v = v.replace(/\bsalami(s)?\b/g, "cured dead animal mélange$1");    
     v = v.replace(/\bPoultry\b/g, "Dead Bird");
-    v = v.replace(/\bpoultry\b/g, "dead bird");    
-    v = v.replace(/\bAnimal (P|p)roduct(s)?\b/g, "Fruit$1 of Cruelty");
+    v = v.replace(/\bpoultry\b/g, "dead bird");  
+    v = v.replace(/\bHogget\b/g, "Dead Sheep");
+    v = v.replace(/\bhogget\b/g, "dead sheep");
+    v = v.replace(/\bMutton\b/g, "Dead Sheep");
+    v = v.replace(/\bmutton\b/g, "dead sheep");
+    v = v.replace(/\bVenison\b/g, "Dead Deer");
+    v = v.replace(/\bvenison\b/g, "dead deer");
+    v = v.replace(/\bLamb (C|c)hop(s)?\b/g, "Dead sheep");
+    v = v.replace(/\blamb chop(s)?\b/g, "dead sheep");
+    v = v.replace(/\bAnimal (P|p)roduct(s)?\b/g, "Fruit$2 of Cruelty");
     v = v.replace(/\banimal product(s)?\b/g, "fruit$1 of cruelty");
-    v = v.replace(/\bTurducken\b/g, "Dead animal sodomy");
-    v = v.replace(/\bturducken\b/g, "dead animal sodomy");
+    v = v.replace(/\bTurducken\b/g, "Dead, sodomized turkey");
+    v = v.replace(/\bturducken\b/g, "dead, sodomized turkey");
+
+    // Carnies
+    v = v.replace(/\bMeat(-| )?(E|e)ater(s)?\b/g, "Omnivore$3");
+    v = v.replace(/\bmeat(-| )?eater(s)?\b/g, "omnivore$2");
+    v = v.replace(/\bCarnivore(s)?\b/g, "Bloodmouth$1");
+    v = v.replace(/\bcarnivore(s)?\b/g, "bloodmouth$1");
+
+    // Vegetarian
 
     // Honey
+    v = v.replace(/\bHoney\b/g, "Product of Exploited and Non-Unionized Bee Labor");
+    v = v.replace(/\bhoney\b/g, "product of exploited and non-unionized bee labor");
 
     // Milk
-
+    v = v.replace(/\bDairy (F|f)arm?\b/g, "Cow Rape Factory");
+    v = v.replace(/\bDairy\b/g, "Cow Titty-Juice");
+    v = v.replace(/\bdairy\b/g, "cow titty-juice");
+    
     // Protein
 
     // B12
+
+    // Eggs
+    v = v.replace(/\bEgg(s)?\b/g, "Prenatal Chicken$1");
+    v = v.replace(/\beggs(s)?\b/g, "prenatal chicken$1");
 
     // Cheese
 
@@ -118,7 +147,17 @@ function replaceText(v)
     v = v.replace(/\bVeganuary\b/g, "The best time of the year");
     v = v.replace(/\bveganuary\b/g, "the best time of the year");
 
+    // Stopped veganism
+    v = v.replace(/\bI('|")m not vegan anymore\b/gi, "I stopped caring about animals and the environment");
+    v = v.replace(/\bI('|")m no longer vegan\b/g, "the best time of the year");
 
+    // Vegan
+    v = v.replace(/\ba Vegan\b/g, "a Morally Superior Person");
+    v = v.replace(/\ba vegan\b/g, "a morally superior person");
+    v = v.replace(/\bVegan(?:(s)\b(')|s\b)/g, "Morally Superior People$2$1");
+    v = v.replace(/\bvegan(?:(s)\b(')|s\b)/g, "morally superior people$2$1");
+    v = v.replace(/\bVegan\b/g, "Moral Superiority");
+    v = v.replace(/\bvegan\b/g, "moral superiority");
     return v;
 }
 
